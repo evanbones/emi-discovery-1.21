221d4684e0638d97523ee5d219f5f72a51c9c27e
Kevin Murphy <kevin@eorbit.net>
2026-02-24 19:27:00 -0800

    Add special handling for rendering and tooltips to List and Tag EmiIngredients

---
+++ common/src/main/java/net/funkpla/emi_discovery/mixin/emi/ListEmiIngredientMixin.java
@@ -0,0 +1,51 @@
+package net.funkpla.emi_discovery.mixin.emi;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import dev.emi.emi.api.stack.EmiIngredient;
+import dev.emi.emi.api.stack.ListEmiIngredient;
+import java.util.List;
+import net.funkpla.emi_discovery.KnownItems;
+import org.jetbrains.annotations.NotNull;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+
+@SuppressWarnings("UnstableApiUsage")
+@Mixin(ListEmiIngredient.class)
+public class ListEmiIngredientMixin {
+  @Unique
+  private @NotNull List<? extends EmiIngredient> filterEmiIngredients(
+      ListEmiIngredient listEmiIngredient) {
+    return listEmiIngredient.getIngredients().stream()
+        .filter(KnownItems::shouldIngredientDisplay)
+        .toList();
+  }
+
+  @WrapOperation(
+      remap = false,
+      method = "render",
+      at =
+          @At(
+              value = "FIELD",
+              target = "Ldev/emi/emi/api/stack/ListEmiIngredient;ingredients:Ljava/util/List;",
+              opcode = Opcodes.GETFIELD))
+  private List<? extends EmiIngredient> filterRenderedStacks(
+      ListEmiIngredient listEmiIngredient, Operation<List<? extends EmiIngredient>> original) {
+    return filterEmiIngredients(listEmiIngredient);
+  }
+
+  @WrapOperation(
+      remap = false,
+      method = "getTooltip",
+      at =
+          @At(
+              value = "FIELD",
+              target = "Ldev/emi/emi/api/stack/ListEmiIngredient;ingredients:Ljava/util/List;",
+              opcode = Opcodes.GETFIELD))
+  private List<? extends EmiIngredient> filterTooltipStacks(
+      ListEmiIngredient listEmiIngredient, Operation<List<? extends EmiIngredient>> original) {
+    return filterEmiIngredients(listEmiIngredient);
+  }
+}
---
+++ common/src/main/java/net/funkpla/emi_discovery/mixin/emi/accessor/TagEmiIngredientAccessor.java
@@ -0,0 +1,15 @@
+package net.funkpla.emi_discovery.mixin.emi.accessor;
+
+import dev.emi.emi.api.stack.EmiStack;
+import dev.emi.emi.api.stack.TagEmiIngredient;
+import dev.emi.emi.recipe.EmiTagRecipe;
+import java.util.List;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@SuppressWarnings("UnstableApiUsage")
+@Mixin(TagEmiIngredient.class)
+public interface TagEmiIngredientAccessor {
+  @Accessor(value ="stacks", remap = false)
+  List<EmiStack> getStacks();
+}
